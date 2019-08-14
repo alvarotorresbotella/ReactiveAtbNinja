fa991ef98c5739b6446616f2ef5139761836a9dd
atbot <atbot@LAPTOP-3C90SCG2>
2019-08-14 23:45:21 +0200

    201908142345 mt

--- src/test/java/com/atb/ninja/ReactorAtbApplicationTests.java
+++ src/test/java/com/atb/ninja/ReactorAtbApplicationTests.java
@@ -5,11 +5,13 @@ import org.junit.runner.RunWith;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.powermock.api.mockito.PowerMockito.mockStatic;
 import static org.powermock.api.mockito.PowerMockito.verifyStatic;
 
 @RunWith(PowerMockRunner.class)
+@SpringBootTest(classes = ReactorAtbApplication.class)
 public class ReactorAtbApplicationTests {
 
 	@Test
